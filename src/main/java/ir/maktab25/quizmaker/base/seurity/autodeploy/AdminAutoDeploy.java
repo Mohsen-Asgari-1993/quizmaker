266a2654ca9e24ca751bbf09fff498792cc6a7d9
Mohsen <mohsen1993asgari@gmail.com>
2019-07-28 15:11:15 +0430

    Creates AdminAutoDeploy

---
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/autodeploy/AdminAutoDeploy.java
@@ -0,0 +1,50 @@
+package ir.maktab25.quizmaker.base.seurity.autodeploy;
+
+import ir.maktab25.quizmaker.base.seurity.domian.BaseUser;
+import ir.maktab25.quizmaker.base.seurity.domian.Role;
+import ir.maktab25.quizmaker.base.seurity.domian.enumeration.RoleName;
+import ir.maktab25.quizmaker.base.seurity.serivce.BaseUserService;
+import ir.maktab25.quizmaker.base.seurity.serivce.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class AdminAutoDeploy {
+
+    @Autowired
+    BaseUserService baseUserService;
+
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void initialize() {
+        Role admin = roleService.findByName(RoleName.ROLE_ADMIN.toString());
+        if (admin == null)
+            admin = roleService.save(new Role(null, RoleName.ROLE_ADMIN, null));
+        if (roleService.findByName(RoleName.ROLE_TEACHER.toString()) == null)
+            roleService.save(new Role(null, RoleName.ROLE_TEACHER, null));
+        if (roleService.findByName(RoleName.ROLE_STUDENT.toString()) == null)
+            roleService.save(new Role(null, RoleName.ROLE_STUDENT, null));
+
+        BaseUser roleAdmin = baseUserService.findByUserName("admin");
+        if (roleAdmin == null) {
+            roleAdmin = new BaseUser();
+            roleAdmin.setUserName("admin");
+            roleAdmin.setPassword(passwordEncoder.encode("maktab25"));
+            roleAdmin.setIsActive(true);
+            Set<Role> roles = new HashSet<>();
+            roles.add(admin);
+            roleAdmin.setRoles(roles);
+            baseUserService.save(roleAdmin);
+        }
+    }
+}
