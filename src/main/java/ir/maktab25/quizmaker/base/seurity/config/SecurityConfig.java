9ee3c15e914fc2aaf55b81fa3f9f03afef00a0fc
Mohsen <mohsen1993asgari@gmail.com>
2019-07-28 14:33:02 +0430

    Creates simple Security config

---
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package ir.maktab25.quizmaker.base.seurity.config;
+
+import ir.maktab25.quizmaker.base.seurity.serivce.impl.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailServiceImpl userDetailService;
+    private final String loginPageUrl = "/form/login.html";
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**")
+                .authenticated()
+                .and()
+                .formLogin()
+//                .loginPage("/form/login.html")
+                .defaultSuccessUrl("/");
+//                .permitAll();
+    }
+}
