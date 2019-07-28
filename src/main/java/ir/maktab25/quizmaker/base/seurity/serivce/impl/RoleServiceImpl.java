fd3c270ced1019b68846927ca212ec4a78e8e912
Mohsen <mohsen1993asgari@gmail.com>
2019-07-28 11:30:02 +0430

    Creates RoleServiceImpl

---
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/serivce/impl/RoleServiceImpl.java
@@ -0,0 +1,22 @@
+package ir.maktab25.quizmaker.base.seurity.serivce.impl;
+
+import ir.maktab25.quizmaker.base.service.impl.BaseServiceImpl;
+import ir.maktab25.quizmaker.base.seurity.domian.Role;
+import ir.maktab25.quizmaker.base.seurity.repository.RoleRepository;
+import ir.maktab25.quizmaker.base.seurity.serivce.RoleService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class RoleServiceImpl extends BaseServiceImpl<Role, Long, RoleRepository> implements RoleService {
+
+    public RoleServiceImpl(RoleRepository baseRepository) {
+        super(baseRepository);
+    }
+
+    @Override
+    public Role findByName(String name) {
+        return baseRepository.findByName(name);
+    }
+}
