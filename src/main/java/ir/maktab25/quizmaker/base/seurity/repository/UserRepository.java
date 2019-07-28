449c1d7857de6986c7664d0b6313d44bbbca9b19
Mohsen <mohsen1993asgari@gmail.com>
2019-07-28 11:23:58 +0430

    Update UserRepository

--- src/main/java/ir/maktab25/quizmaker/base/seurity/repository/UserRepository.java
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package ir.maktab25.quizmaker.base.seurity.repository;
 
-public interface UserRepository {
+import ir.maktab25.quizmaker.base.seurity.domian.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUserName(String username);
 }
