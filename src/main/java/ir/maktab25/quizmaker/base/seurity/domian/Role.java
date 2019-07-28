ba8cf550b3678cb3ac09111cfbdc31ab9bdf245f
Mohsen <mohsen1993asgari@gmail.com>
2019-07-28 11:17:47 +0430

    Creates User And Role

---
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/domian/Role.java
@@ -0,0 +1,41 @@
+package ir.maktab25.quizmaker.base.seurity.domian;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import ir.maktab25.quizmaker.base.domain.BaseEntity;
+import lombok.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A Role.
+ */
+@Entity
+@Table(name = "role")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Role extends BaseEntity<Long> implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    private Set<User> usersYas = new HashSet<>();
+
+}
