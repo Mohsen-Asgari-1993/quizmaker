f21fab1e89463d97d785f7b37ca20a4598041ea9
Mohsen <mohsen1993asgari@gmail.com>
2019-07-28 11:25:18 +0430

    Creates RoleRepository

---
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package ir.maktab25.quizmaker.base.seurity.repository;
+
+import ir.maktab25.quizmaker.base.seurity.domian.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
