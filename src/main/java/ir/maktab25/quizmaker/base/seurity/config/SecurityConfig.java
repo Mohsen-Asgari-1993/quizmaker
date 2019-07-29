04787182849a4b58d4fd6d0355764acc1b88dfd5
Mohsen <mohsen1993asgari@gmail.com>
2019-07-29 16:48:25 +0430

    Update SecurityConfig

--- src/main/java/ir/maktab25/quizmaker/base/seurity/config/SecurityConfig.java
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/config/SecurityConfig.java
@@ -39,10 +39,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**")
-                .authenticated()
+                .antMatchers("/form/**")
+                .permitAll().anyRequest().authenticated()
                 .and()
                 .formLogin()
+                .loginPage(loginPageUrl)
                 .permitAll()
                 .and()
                 .formLogin()
