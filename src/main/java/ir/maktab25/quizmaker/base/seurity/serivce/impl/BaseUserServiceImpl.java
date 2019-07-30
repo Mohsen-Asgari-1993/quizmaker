0223c16f748ba2b59a73a25d886124bf5ac84f4d
Mohsen <mohsen1993asgari@gmail.com>
2019-07-30 11:33:07 +0430

    update BaseUserServiceImpl impls saveTeacher and saveStudent

--- src/main/java/ir/maktab25/quizmaker/base/seurity/serivce/impl/BaseUserServiceImpl.java
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/serivce/impl/BaseUserServiceImpl.java
@@ -2,10 +2,17 @@ package ir.maktab25.quizmaker.base.seurity.serivce.impl;
 
 import ir.maktab25.quizmaker.base.service.impl.BaseServiceImpl;
 import ir.maktab25.quizmaker.base.seurity.domian.BaseUser;
+import ir.maktab25.quizmaker.base.seurity.domian.Role;
+import ir.maktab25.quizmaker.base.seurity.domian.enumeration.RoleName;
 import ir.maktab25.quizmaker.base.seurity.repository.BaseUserRepository;
 import ir.maktab25.quizmaker.base.seurity.serivce.BaseUserService;
+import ir.maktab25.quizmaker.base.seurity.serivce.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -15,8 +22,29 @@ public class BaseUserServiceImpl extends BaseServiceImpl<BaseUser, Long, BaseUse
         super(baseRepository);
     }
 
+    @Autowired
+    RoleService roleService;
+
     @Override
     public BaseUser findByUserName(String username) {
         return baseRepository.findByUserName(username);
+    }
+
+    @Override
+    public BaseUser saveTeacher(BaseUser baseUser) {
+        Role role = roleService.findByName(RoleName.ROLE_TEACHER);
+        Set<Role> roleSet = new HashSet<>();
+        roleSet.add(role);
+        baseUser.setRoles(roleSet);
+        return super.save(baseUser);
+    }
+
+    @Override
+    public BaseUser saveStudent(BaseUser baseUser) {
+        Role role = roleService.findByName(RoleName.ROLE_STUDENT);
+        Set<Role> roleSet = new HashSet<>();
+        roleSet.add(role);
+        baseUser.setRoles(roleSet);
+        return super.save(baseUser);
     }
 }
