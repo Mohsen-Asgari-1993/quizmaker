120477a73365d7d08c37c17b5b0ecf707556c7c1
Mohsen <mohsen1993asgari@gmail.com>
2019-07-30 11:33:55 +0430

    update BaseUserResource

---
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/rest/BaseUserResource.java
@@ -0,0 +1,45 @@
+package ir.maktab25.quizmaker.base.seurity.rest;
+
+import ir.maktab25.quizmaker.base.rest.BaseRestFulService;
+import ir.maktab25.quizmaker.base.seurity.domian.BaseUser;
+import ir.maktab25.quizmaker.base.seurity.serivce.BaseUserService;
+import ir.maktab25.quizmaker.service.dto.BaseUserDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/BaseUser")
+public class BaseUserResource extends BaseRestFulService<BaseUser, BaseUserDTO, Long, BaseUserService> {
+
+    public BaseUserResource(BaseUserService baseService) {
+        super(baseService);
+    }
+
+    @PostMapping("/teacher")
+    public ResponseEntity<BaseUserDTO> createTeacher(@RequestBody BaseUserDTO userDTO) {
+        if (userDTO.getId() != null) {
+            return ResponseEntity
+                    .badRequest()
+                    .header("id exists", "A new entity cannot already have an ID")
+                    .build();
+        }
+        BaseUser baseUser = baseService.saveTeacher(baseMapper.toEntity(userDTO));
+        return ResponseEntity.ok(baseMapper.toDTO(baseUser));
+    }
+
+    @PostMapping("/student")
+    public ResponseEntity<BaseUserDTO> createStudent(@RequestBody BaseUserDTO userDTO) {
+        if (userDTO.getId() != null) {
+            return ResponseEntity
+                    .badRequest()
+                    .header("id exists", "A new entity cannot already have an ID")
+                    .build();
+        }
+        BaseUser baseUser = baseService.saveStudent(baseMapper.toEntity(userDTO));
+        return ResponseEntity.ok(baseMapper.toDTO(baseUser));
+    }
+
+}
