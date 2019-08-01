e33e1acca80c04daa24ce0c8ce0978cf0eb8ea96
Mohsen <mohsen1993asgari@gmail.com>
2019-08-01 12:34:38 +0430

    update BaseUserResource impls enable method

--- src/main/java/ir/maktab25/quizmaker/base/seurity/rest/BaseUserResource.java
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/rest/BaseUserResource.java
@@ -5,12 +5,8 @@ import ir.maktab25.quizmaker.base.seurity.domian.BaseUser;
 import ir.maktab25.quizmaker.base.seurity.serivce.BaseUserService;
 import ir.maktab25.quizmaker.service.dto.BaseUserDTO;
 import ir.maktab25.quizmaker.service.mapper.BaseUserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/BaseUser")
@@ -23,7 +19,7 @@ public class BaseUserResource extends BaseRestFulService<BaseUser, BaseUserDTO, 
 
 
     @PostMapping("/save")
-    public ResponseEntity<BaseUserDTO> createTeacher(@RequestBody BaseUserDTO userDTO) {
+    public ResponseEntity<BaseUserDTO> createUser(@RequestBody BaseUserDTO userDTO) {
         if (userDTO.getId() != null) {
             return ResponseEntity
                     .badRequest()
@@ -34,28 +30,10 @@ public class BaseUserResource extends BaseRestFulService<BaseUser, BaseUserDTO, 
         return ResponseEntity.ok(baseMapper.toDTO(baseUser));
     }
 
-//    @PostMapping("/teacher")
-//    public ResponseEntity<BaseUserDTO> createTeacher(@RequestBody BaseUserDTO userDTO) {
-//        if (userDTO.getId() != null) {
-//            return ResponseEntity
-//                    .badRequest()
-//                    .header("id exists", "A new entity cannot already have an ID")
-//                    .build();
-//        }
-//        BaseUser baseUser = baseService.saveTeacher(baseMapper.toEntity(userDTO));
-//        return ResponseEntity.ok(baseMapper.toDTO(baseUser));
-//    }
-//
-//    @PostMapping("/student")
-//    public ResponseEntity<BaseUserDTO> createStudent(@RequestBody BaseUserDTO userDTO) {
-//        if (userDTO.getId() != null) {
-//            return ResponseEntity
-//                    .badRequest()
-//                    .header("id exists", "A new entity cannot already have an ID")
-//                    .build();
-//        }
-//        BaseUser baseUser = baseService.saveStudent(baseMapper.toEntity(userDTO));
-//        return ResponseEntity.ok(baseMapper.toDTO(baseUser));
-//    }
+    @GetMapping("/enable/{id}")
+    public ResponseEntity<BaseUserDTO> enableUser(@PathVariable Long id) {
+        BaseUser baseUser = baseService.enable(id);
+        return ResponseEntity.ok(baseMapper.toDTO(baseUser));
+    }
 
 }
