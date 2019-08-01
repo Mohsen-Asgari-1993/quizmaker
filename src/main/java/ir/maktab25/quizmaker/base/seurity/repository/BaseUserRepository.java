8fc554fd922d4e3349c3e289a6cf4ed7fdee776f
Mohsen <mohsen1993asgari@gmail.com>
2019-08-01 12:36:52 +0430

    update BaseUserRepository define findByIsActive method

--- src/main/java/ir/maktab25/quizmaker/base/seurity/repository/BaseUserRepository.java
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/repository/BaseUserRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BaseUserRepository extends JpaRepository<BaseUser, Long> {
 
     @Query("select u from BaseUser u left join fetch u.roles where u.userName = :username")
     BaseUser findByUserName(@Param("username") String username);
+
+    List<BaseUser> findAllByIsActive(Boolean isActive);
 }
