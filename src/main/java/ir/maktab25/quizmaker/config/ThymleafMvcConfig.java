c93c554eed1b8c1ec8d1622ae6e771f0eeeaf9d3
Mohsen <mohsen1993asgari@gmail.com>
2019-08-03 14:13:14 +0430

    Creates ThymleafMvcConfig

---
+++ src/main/java/ir/maktab25/quizmaker/config/ThymleafMvcConfig.java
@@ -0,0 +1,64 @@
+package ir.maktab25.quizmaker.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ir.maktab25")
+public class ThymleafMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/form/**").addResourceLocations("/form/");
+//        registry.addResourceHandler("/css/**").addResourceLocations("/home/css/");
+//        registry.addResourceHandler("/fonts/**").addResourceLocations("/home/fonts/");
+//        registry.addResourceHandler("/img/**").addResourceLocations("/home/img/");
+//        registry.addResourceHandler("/js/**").addResourceLocations("/home/js/");
+//        registry.addResourceHandler("/master.style-switcher/**").addResourceLocations("/home/master.style-switcher/");
+//        registry.addResourceHandler("/vendor/**").addResourceLocations("/home/vendor/");
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    /*
+     * STEP 2 - Create SpringTemplateEngine
+     * */
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    /*
+     * STEP 3 - Register ThymeleafViewResolver
+     * */
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
+    }
+}
