d1e2923bbe9b894f88d23c8d6c9145134d2d3e9f
Mohsen <mohsen1993asgari@gmail.com>
2019-08-03 16:59:25 +0430

    i have no idea what i've done

--- src/main/java/ir/maktab25/quizmaker/base/seurity/config/SecurityConfig.java
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/templates/**")
                 .permitAll()
-                .antMatchers("/form/**")
+                .antMatchers("/webapp/**","/form/**")
                 .permitAll()
                 .antMatchers("/BaseUser/save")
                 .permitAll();
