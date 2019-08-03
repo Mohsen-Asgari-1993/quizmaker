8686ef386c1c48ce10eef1f4a0ececea7778b2fa
Mohsen <mohsen1993asgari@gmail.com>
2019-08-03 14:44:22 +0430

    update path

--- src/main/java/ir/maktab25/quizmaker/controller/util/Path.java
+++ src/main/java/ir/maktab25/quizmaker/controller/util/Path.java
@@ -2,10 +2,10 @@ package ir.maktab25.quizmaker.controller.util;
 
 public class Path {
 
-    public final String REDIRECT = "/redirect";
-    public final String ADMIN= "/admin";
-    public final String STUDENT = "/student";
-    public final String TEACHER = "/teacher";
-    public final String COURSE = "/course";
-    public final String EXAM = "/exam";
+    public static final String REDIRECT = "/redirect";
+    public static final String ADMIN= "/admin";
+    public static final String STUDENT = "/student";
+    public static final String TEACHER = "/teacher";
+    public static final String COURSE = "/course";
+    public static final String EXAM = "/exam";
 }
