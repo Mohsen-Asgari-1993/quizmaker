907a1dbefd81df3f8d2897e52ce5b42e779c1580
Mohsen <mohsen1993asgari@gmail.com>
2019-08-03 14:43:28 +0430

    Creates Path Class

---
+++ src/main/java/ir/maktab25/quizmaker/controller/util/Path.java
@@ -0,0 +1,11 @@
+package ir.maktab25.quizmaker.controller.util;
+
+public class Path {
+
+    public final String REDIRECT = "/redirect";
+    public final String ADMIN= "/admin";
+    public final String STUDENT = "/student";
+    public final String TEACHER = "/teacher";
+    public final String COURSE = "/course";
+    public final String EXAM = "/exam";
+}
