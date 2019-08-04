68032ce0f02c1c646a0350c306f3afdd0959cb8e
Mohsen <mohsen1993asgari@gmail.com>
2019-08-04 12:22:21 +0430

    updates BaseUserRepository

--- src/main/java/ir/maktab25/quizmaker/base/seurity/repository/BaseUserRepository.java
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/repository/BaseUserRepository.java
@@ -2,22 +2,15 @@ package ir.maktab25.quizmaker.base.seurity.repository;
 
 import ir.maktab25.quizmaker.base.seurity.domian.BaseUser;
 import ir.maktab25.quizmaker.base.seurity.domian.enumeration.RoleName;
+import ir.maktab25.quizmaker.repository.base.UserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BaseUserRepository extends JpaRepository<BaseUser, Long> {
+public interface BaseUserRepository extends UserRepository<BaseUser, Long> {
 
 //    @Query("select u from BaseUser u left join fetch u.roles where u.userName = :username")
-    BaseUser findByUserName(String username);
 
-    List<BaseUser> findAllByIsActive(Boolean isActive);
-
-    List<BaseUser> findAllByRoles_RoleName(RoleName roleName);
-
-    List<BaseUser> findAllByFirstName(String firstName);
-
-    List<BaseUser> findAllByLastName(String lastName);
 }
