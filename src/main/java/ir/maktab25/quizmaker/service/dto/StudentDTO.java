86e0a041270307cb985ad185384578d12978728a
Mohsen <mohsen1993asgari@gmail.com>
2019-08-04 14:01:25 +0430

    Creates StudentDTO

---
+++ src/main/java/ir/maktab25/quizmaker/service/dto/StudentDTO.java
@@ -0,0 +1,14 @@
+package ir.maktab25.quizmaker.service.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class StudentDTO extends UserDTO {
+
+    private Long id;
+
+}
