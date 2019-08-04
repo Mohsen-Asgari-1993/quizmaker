040ccfdce753cce282747da7cc0e8ffb7ec819d4
Mohsen <mohsen1993asgari@gmail.com>
2019-08-04 12:25:24 +0430

    updates BaseUserService

--- src/main/java/ir/maktab25/quizmaker/base/seurity/serivce/BaseUserService.java
+++ src/main/java/ir/maktab25/quizmaker/base/seurity/serivce/BaseUserService.java
@@ -1,23 +1,8 @@
 package ir.maktab25.quizmaker.base.seurity.serivce;
 
-import ir.maktab25.quizmaker.base.service.BaseService;
 import ir.maktab25.quizmaker.base.seurity.domian.BaseUser;
+import ir.maktab25.quizmaker.service.base.UserService;
 
-import java.util.List;
-
-public interface BaseUserService extends BaseService<BaseUser, Long> {
-
-    BaseUser findByUserName(String username);
-
-    BaseUser enable(Long id);
-
-    List<BaseUser> findAllByIsActive(Boolean isActive);
-
-
-    List<BaseUser> findAllByRoleName(String roleName);
-
-    List<BaseUser> findAllByFirstName(String firstName);
-
-    List<BaseUser> findAllByLastName(String lastName);
+public interface BaseUserService extends UserService<BaseUser, Long> {
 
 }
