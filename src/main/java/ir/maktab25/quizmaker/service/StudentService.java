6685d86c662caf1bceadd7aa159fdb5c539047e0
Mohsen <mohsen1993asgari@gmail.com>
2019-08-04 14:06:19 +0430

    Creates StudentService

---
+++ src/main/java/ir/maktab25/quizmaker/service/StudentService.java
@@ -0,0 +1,7 @@
+package ir.maktab25.quizmaker.service;
+
+import ir.maktab25.quizmaker.domain.Student;
+import ir.maktab25.quizmaker.service.base.BasicUserService;
+
+public interface StudentService extends BasicUserService<Student, Long> {
+}
