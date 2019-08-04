403774baabc5dd51712ddadaab00d08db7683f40
Mohsen <mohsen1993asgari@gmail.com>
2019-08-04 14:27:23 +0430

    updates adminController and admin.html

--- src/main/java/ir/maktab25/quizmaker/controller/AdminController.java
+++ src/main/java/ir/maktab25/quizmaker/controller/AdminController.java
@@ -1,7 +1,9 @@
 package ir.maktab25.quizmaker.controller;
 
 import ir.maktab25.quizmaker.rest.CourseResource;
+import ir.maktab25.quizmaker.rest.TeacherResource;
 import ir.maktab25.quizmaker.service.dto.CourseDTO;
+import ir.maktab25.quizmaker.service.dto.TeacherDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,10 +19,22 @@ public class AdminController {
     @Autowired
     CourseResource courseResource;
 
+    @Autowired
+    TeacherResource teacherResource;
+
     @GetMapping("/course")
     public String getCourses(Model model) {
         List<CourseDTO> body = courseResource.getAllNotPageable().getBody();
-        model.addAttribute("courses", body );
+        model.addAttribute("courses", body);
         return "adminCourse";
+    }
+
+    @GetMapping("/teacher")
+    public String getTeachers(Model model) {
+        List<TeacherDTO> enables = teacherResource.findAllEnables().getBody();
+        List<TeacherDTO> disables = teacherResource.findAllDisable().getBody();
+        model.addAttribute("enables", enables);
+        model.addAttribute("disables", disables);
+        return "adminTeachers";
     }
 }
