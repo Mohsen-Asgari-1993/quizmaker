9fa0d5074bdd41f8e92412879557800781cd618f
Mohsen <mohsen1993asgari@gmail.com>
2019-08-04 14:35:35 +0430

    update TeacherServiceImpl override save method

--- src/main/java/ir/maktab25/quizmaker/service/impl/TeacherServiceImpl.java
+++ src/main/java/ir/maktab25/quizmaker/service/impl/TeacherServiceImpl.java
@@ -1,17 +1,26 @@
 package ir.maktab25.quizmaker.service.impl;
 
+import ir.maktab25.quizmaker.base.seurity.domian.Role;
+import ir.maktab25.quizmaker.base.seurity.domian.enumeration.RoleName;
+import ir.maktab25.quizmaker.base.seurity.serivce.RoleService;
 import ir.maktab25.quizmaker.domain.Teacher;
 import ir.maktab25.quizmaker.repository.TeacherRepository;
 import ir.maktab25.quizmaker.service.TeacherService;
 import ir.maktab25.quizmaker.service.impl.base.BasicUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @Transactional
 public class TeacherServiceImpl extends BasicUserServiceImpl<Teacher, Long, TeacherRepository> implements TeacherService {
+
+    @Autowired
+    RoleService roleService;
 
     public TeacherServiceImpl(TeacherRepository baseRepository) {
         super(baseRepository);
@@ -20,5 +29,15 @@ public class TeacherServiceImpl extends BasicUserServiceImpl<Teacher, Long, Teac
     @Override
     public List<Teacher> findAllByCode(String code) {
         return baseRepository.findAllByCode(code);
+    }
+
+    @Override
+    public Teacher save(Teacher t) {
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleService.findByName(RoleName.TEACHER));
+        t.setRoles(roles);
+        if (t.getIsActive() == null)
+            t.setIsActive(false);
+        return super.save(t);
     }
 }
