f1822e3ae0a3885a5a35a90072c23c74e836be73
Mohsen <mohsen1993asgari@gmail.com>
2019-08-04 14:34:33 +0430

    update StudentServiceImpl override save method

--- src/main/java/ir/maktab25/quizmaker/service/impl/StudentServiceImpl.java
+++ src/main/java/ir/maktab25/quizmaker/service/impl/StudentServiceImpl.java
@@ -1,17 +1,37 @@
 package ir.maktab25.quizmaker.service.impl;
 
+import ir.maktab25.quizmaker.base.seurity.domian.Role;
+import ir.maktab25.quizmaker.base.seurity.domian.enumeration.RoleName;
+import ir.maktab25.quizmaker.base.seurity.serivce.RoleService;
 import ir.maktab25.quizmaker.domain.Student;
 import ir.maktab25.quizmaker.repository.StudentRepository;
 import ir.maktab25.quizmaker.service.StudentService;
 import ir.maktab25.quizmaker.service.impl.base.BasicUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Transactional
 public class StudentServiceImpl extends BasicUserServiceImpl<Student, Long, StudentRepository> implements StudentService {
 
+    @Autowired
+    RoleService roleService;
+
     public StudentServiceImpl(StudentRepository baseRepository) {
         super(baseRepository);
+    }
+
+    @Override
+    public Student save(Student t) {
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleService.findByName(RoleName.STUDENT));
+        t.setRoles(roles);
+        if (t.getIsActive() == null)
+            t.setIsActive(false);
+        return super.save(t);
     }
 }
