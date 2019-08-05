5ed24038a2f24b110b4b97737cb3d42ea407e063
Mohsen <mohsen1993asgari@gmail.com>
2019-08-05 10:39:06 +0430

    update AdminController adds enable and delete path

--- src/main/java/ir/maktab25/quizmaker/controller/AdminController.java
+++ src/main/java/ir/maktab25/quizmaker/controller/AdminController.java
@@ -1,5 +1,6 @@
 package ir.maktab25.quizmaker.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import ir.maktab25.quizmaker.rest.CourseResource;
 import ir.maktab25.quizmaker.rest.TeacherResource;
 import ir.maktab25.quizmaker.service.dto.CourseDTO;
@@ -32,22 +33,28 @@ public class AdminController {
 
     @GetMapping("/teacher")
     public String getTeachers(Model model) {
-        List<TeacherDTO> enables = teacherResource.findAllEnables().getBody();
-        List<TeacherDTO> disables = teacherResource.findAllDisable().getBody();
-        model.addAttribute("enables", enables);
-        model.addAttribute("disables", disables);
+        bindDateForAdminTeachers(model);
         return "adminTeachers";
     }
 
     @GetMapping("/enable/{id}")
-    public String enable(@PathVariable Long id){
+    public String enable(@PathVariable Long id, Model model){
         teacherResource.enableUser(id);
+        bindDateForAdminTeachers(model);
         return "adminTeachers";
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Long id){
+    public String delete(@PathVariable Long id, Model model){
         teacherResource.deleteById(id);
+        bindDateForAdminTeachers(model);
         return "adminTeachers";
+    }
+
+    private void bindDateForAdminTeachers(Model model){
+        List<TeacherDTO> enables = teacherResource.findAllEnables().getBody();
+        List<TeacherDTO> disables = teacherResource.findAllDisable().getBody();
+        model.addAttribute("enables", enables);
+        model.addAttribute("disables", disables);
     }
 }
