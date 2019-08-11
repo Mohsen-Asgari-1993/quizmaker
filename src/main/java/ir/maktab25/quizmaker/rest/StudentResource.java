a99ab52ddadf8ab55c8a6208d1c0ef37f3decfa8
Mohsen-Asgari-1993 <mohsen1993asgari@gmial.com>
2019-08-11 23:31:56 +0430

    update StudentResource define changeRole method

--- src/main/java/ir/maktab25/quizmaker/rest/StudentResource.java
+++ src/main/java/ir/maktab25/quizmaker/rest/StudentResource.java
@@ -1,10 +1,16 @@
 package ir.maktab25.quizmaker.rest;
 
 import ir.maktab25.quizmaker.domain.Student;
+import ir.maktab25.quizmaker.domain.Teacher;
 import ir.maktab25.quizmaker.rest.base.BasicUserResource;
 import ir.maktab25.quizmaker.service.StudentService;
 import ir.maktab25.quizmaker.service.dto.StudentDTO;
+import ir.maktab25.quizmaker.service.dto.TeacherDTO;
 import ir.maktab25.quizmaker.service.mapper.StudentMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +20,12 @@ public class StudentResource extends BasicUserResource<Student, StudentDTO, Long
 
     public StudentResource(StudentService baseService, StudentMapper baseMapper) {
         super(baseService, baseMapper);
+    }
+
+    @PostMapping("/changeRole")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<StudentDTO> changeRole(@RequestBody StudentDTO studentDTO) {
+        Student student = baseService.changeRole(baseMapper.toEntity(studentDTO));
+        return ResponseEntity.ok(baseMapper.toDTO(student));
     }
 }
