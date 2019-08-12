141708aa976e5f028b952bed95f13bb15acff2e3
Mohsen-Asgari-1993 <mohsen1993asgari@gmial.com>
2019-08-12 14:14:15 +0430

    Update QuizRepository define findAllByTeacher_Id

--- src/main/java/ir/maktab25/quizmaker/repository/QuizRepository.java
+++ src/main/java/ir/maktab25/quizmaker/repository/QuizRepository.java
@@ -4,6 +4,10 @@ import ir.maktab25.quizmaker.domain.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+
+    List<Quiz> findAllByTeacher_Id(Long id);
 }
