dc6170c190a28080d6b83366487bd1602da7d2cd
Mohsen-Asgari-1993 <mohsen1993asgari@gmial.com>
2019-08-12 14:15:37 +0430

    Update QuizServiceImpl Impl findAllByTeacherId

--- src/main/java/ir/maktab25/quizmaker/service/impl/QuizServiceImpl.java
+++ src/main/java/ir/maktab25/quizmaker/service/impl/QuizServiceImpl.java
@@ -7,11 +7,18 @@ import ir.maktab25.quizmaker.service.QuizService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class QuizServiceImpl extends BaseServiceImpl<Quiz, Long, QuizRepository> implements QuizService {
 
     public QuizServiceImpl(QuizRepository baseRepository) {
         super(baseRepository);
+    }
+
+    @Override
+    public List<Quiz> findAllByTeacherId(Long id) {
+        return baseRepository.findAllByTeacher_Id(id);
     }
 }
