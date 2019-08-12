c40b9c662f030ed15a0a90d0d7c2b59406c3be6b
Mohsen-Asgari-1993 <mohsen1993asgari@gmial.com>
2019-08-12 14:03:12 +0430

    Creates QuizDTO

---
+++ src/main/java/ir/maktab25/quizmaker/service/dto/QuizDTO.java
@@ -0,0 +1,28 @@
+package ir.maktab25.quizmaker.service.dto;
+
+import ir.maktab25.quizmaker.base.dto.BaseDTO;
+import ir.maktab25.quizmaker.base.seurity.domian.User;
+import lombok.*;
+
+import java.util.Set;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class QuizDTO extends BaseDTO<Long> {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String timer;
+
+    private User teacher;
+
+    private Set<User> students;
+}
