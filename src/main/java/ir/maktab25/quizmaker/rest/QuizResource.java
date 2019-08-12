a489f9a5eb1aee2a83502772e54bc4a507092e9c
Mohsen-Asgari-1993 <mohsen1993asgari@gmial.com>
2019-08-12 14:18:01 +0430

    Update QuizResource define findAllByTeacherId

--- src/main/java/ir/maktab25/quizmaker/rest/QuizResource.java
+++ src/main/java/ir/maktab25/quizmaker/rest/QuizResource.java
@@ -5,8 +5,13 @@ import ir.maktab25.quizmaker.domain.Quiz;
 import ir.maktab25.quizmaker.service.QuizService;
 import ir.maktab25.quizmaker.service.dto.QuizDTO;
 import ir.maktab25.quizmaker.service.mapper.QuizMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Quiz")
@@ -14,5 +19,11 @@ public class QuizResource extends BaseRestFulService<Quiz, QuizDTO, Long, QuizSe
 
     public QuizResource(QuizService baseService, QuizMapper baseMapper) {
         super(baseService, baseMapper);
+    }
+
+    @GetMapping("/findAllByTeacherId/{id}")
+    public ResponseEntity<List<QuizDTO>> findAllByTeacherId(@PathVariable Long id) {
+        List<Quiz> quizList = baseService.findAllByTeacherId(id);
+        return ResponseEntity.ok(baseMapper.entityToDTOList(quizList));
     }
 }
