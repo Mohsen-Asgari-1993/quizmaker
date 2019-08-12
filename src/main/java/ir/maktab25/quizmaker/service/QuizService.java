abea8b95d1d5393e553ac21079883fde2e5d1146
Mohsen-Asgari-1993 <mohsen1993asgari@gmial.com>
2019-08-12 14:15:07 +0430

    Update QuizService define findAllByTeacherId

--- src/main/java/ir/maktab25/quizmaker/service/QuizService.java
+++ src/main/java/ir/maktab25/quizmaker/service/QuizService.java
@@ -3,5 +3,9 @@ package ir.maktab25.quizmaker.service;
 import ir.maktab25.quizmaker.base.service.BaseService;
 import ir.maktab25.quizmaker.domain.Quiz;
 
+import java.util.List;
+
 public interface QuizService extends BaseService<Quiz, Long> {
+
+    List<Quiz> findAllByTeacherId(Long id);
 }
