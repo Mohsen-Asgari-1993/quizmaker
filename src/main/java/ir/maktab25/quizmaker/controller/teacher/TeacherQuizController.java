7fa3b2b1daff2db5d2a8b3bea4c22cc4cdbef415
Mohsen-Asgari-1993 <mohsen1993asgari@gmial.com>
2019-08-14 10:31:12 +0430

    update TeacherQuizController define addQuiz and deleteQuiz

--- src/main/java/ir/maktab25/quizmaker/controller/teacher/TeacherQuizController.java
+++ src/main/java/ir/maktab25/quizmaker/controller/teacher/TeacherQuizController.java
@@ -18,8 +18,27 @@ public class TeacherQuizController {
 
     @GetMapping("/{id}")
     public String getQuizzes(@PathVariable Long id, Model model) {
-        model.addAttribute("quizzes", courseResource.getById(id).getBody().getQuizzes());
-        model.addAttribute("quiz", new QuizDTO());
+       bindDataForTeacherQuiz(id, model);
         return "teacherQuiz";
+    }
+
+    @GetMapping("/addQuiz/{id}")
+    public String addQuiz(@PathVariable Long id, Model model, QuizDTO quizDTO) {
+        courseResource.addQuiz(quizDTO, id);
+        bindDataForTeacherQuiz(id, model);
+        return "teacherQuiz";
+    }
+
+    @GetMapping("/addQuiz/{courseId}/{quizId}")
+    public String deleteQuiz(@PathVariable Long courseId,@PathVariable Long quizId, Model model) {
+        courseResource.deleteQuiz(courseId, quizId);
+        bindDataForTeacherQuiz(courseId, model);
+        return "teacherQuiz";
+    }
+
+    private void bindDataForTeacherQuiz(Long id, Model model){
+        model.addAttribute("quizzes", courseResource.getById(id).getBody().getQuizzes());
+        model.addAttribute("quizDTO", new QuizDTO());
+        model.addAttribute("courseId", id);
     }
 }
