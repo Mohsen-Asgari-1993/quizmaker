29dd0a7fc468b95872a6fdbe7c98b2c2b3da29d3
Mohsen <mohsen1993asgari@gmail.com>
2019-08-15 11:12:14 +0430

    update BaseQuestionService

---
+++ src/main/java/ir/maktab25/quizmaker/service/base/BaseQuestionRepository.java
@@ -0,0 +1,8 @@
+package ir.maktab25.quizmaker.service.base;
+
+import ir.maktab25.quizmaker.base.service.BaseService;
+
+import java.io.Serializable;
+
+public interface BaseQuestionRepository<E, PK extends Serializable> extends BaseService<E, PK> {
+}
