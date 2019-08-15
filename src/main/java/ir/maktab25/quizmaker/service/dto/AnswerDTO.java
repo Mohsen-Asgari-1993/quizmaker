cdc41269e2d2401c4bcc661855e916399d5c7b7d
Mohsen <mohsen1993asgari@gmail.com>
2019-08-15 10:44:36 +0430

    Creates AnswerDTO

---
+++ src/main/java/ir/maktab25/quizmaker/service/dto/AnswerDTO.java
@@ -0,0 +1,16 @@
+package ir.maktab25.quizmaker.service.dto;
+
+import ir.maktab25.quizmaker.base.dto.BaseDTO;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class AnswerDTO extends BaseDTO<Long> {
+
+    private Long id;
+
+    private String title;
+}
