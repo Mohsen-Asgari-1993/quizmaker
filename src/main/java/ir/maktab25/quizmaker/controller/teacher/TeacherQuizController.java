29efd6747d1ffceefe640775cf6e9dbbf20c1f2f
Mohsen <mohsen1993asgari@gmail.com>
2019-08-15 10:01:51 +0430

    update TeacherQuizController define method for update a quiz

--- src/main/java/ir/maktab25/quizmaker/controller/teacher/TeacherQuizController.java
+++ src/main/java/ir/maktab25/quizmaker/controller/teacher/TeacherQuizController.java
@@ -1,6 +1,7 @@
 package ir.maktab25.quizmaker.controller.teacher;
 
 import ir.maktab25.quizmaker.rest.CourseResource;
+import ir.maktab25.quizmaker.rest.QuizResource;
 import ir.maktab25.quizmaker.service.dto.QuizDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,9 +18,12 @@ public class TeacherQuizController {
     @Autowired
     CourseResource courseResource;
 
+    @Autowired
+    QuizResource quizResource;
+
     @GetMapping("/{id}")
     public String getQuizzes(@PathVariable Long id, Model model) {
-       bindDataForTeacherQuiz(id, model);
+        bindDataForTeacherQuiz(id, model);
         return "teacherQuiz";
     }
 
@@ -32,15 +36,37 @@ public class TeacherQuizController {
     }
 
     @GetMapping("/delete/{courseId}/{quizId}")
-    public String deleteQuiz(@PathVariable Long courseId,@PathVariable Long quizId, Model model) {
+    public String deleteQuiz(@PathVariable Long courseId, @PathVariable Long quizId, Model model) {
         courseResource.deleteQuiz(courseId, quizId);
         bindDataForTeacherQuiz(courseId, model);
         return "teacherQuiz";
     }
 
-    private void bindDataForTeacherQuiz(Long id, Model model){
+    @GetMapping("/show/{id}")
+    public String showQuiz(@PathVariable Long id, Model model) {
+        bindDataForSingleQuizPage(id, model);
+        return "teacherSingleQuiz";
+    }
+
+    @PostMapping("/updateQuiz/{courseId}/{quizId}")
+    public String updateQuiz(@PathVariable("courseId") Long courseId,
+                             @PathVariable("quizId") Long quizId,
+                             Model model, QuizDTO quizDTO) {
+        quizDTO.setId(quizId);
+        quizResource.update(quizDTO);
+        bindDataForTeacherQuiz(courseId, model);
+        return "teacherQuiz";
+    }
+
+
+    private void bindDataForTeacherQuiz(Long id, Model model) {
         model.addAttribute("quizzes", courseResource.getById(id).getBody().getQuizzes());
         model.addAttribute("quizDTO", new QuizDTO());
+        model.addAttribute("courseId", id);
+    }
+
+    private void bindDataForSingleQuizPage(Long id, Model model) {
+        model.addAttribute("quiz", quizResource.getById(id).getBody());
         model.addAttribute("courseId", id);
     }
 }
