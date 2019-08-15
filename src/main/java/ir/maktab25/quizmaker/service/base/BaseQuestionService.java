395d0354b650f9f00158aaaebdc1f2cb05604528
Mohsen <mohsen1993asgari@gmail.com>
2019-08-15 12:04:35 +0430

    update BaseQuestionService and his impl impl findAllByTeacherUserName Method

--- src/main/java/ir/maktab25/quizmaker/service/base/BaseQuestionService.java
+++ src/main/java/ir/maktab25/quizmaker/service/base/BaseQuestionService.java
@@ -3,6 +3,8 @@ package ir.maktab25.quizmaker.service.base;
 import ir.maktab25.quizmaker.base.service.BaseService;
 
 import java.io.Serializable;
+import java.util.List;
 
 public interface BaseQuestionService<E, PK extends Serializable> extends BaseService<E, PK> {
+    List<E> findAllByTeacherUsername(String username);
 }
