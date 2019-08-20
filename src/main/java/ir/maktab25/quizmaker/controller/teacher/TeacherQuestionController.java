e9cde4a991fbd2a6518721bc8a388b245b25ffc9
Mohsen <mohsen1993asgari@gmail.com>
2019-08-20 10:04:48 +0430

    update TeacherQuestionController define addQuestionPage method and util bindDateForAddQuestionPage method

--- src/main/java/ir/maktab25/quizmaker/controller/teacher/TeacherQuestionController.java
+++ src/main/java/ir/maktab25/quizmaker/controller/teacher/TeacherQuestionController.java
@@ -1,6 +1,9 @@
 package ir.maktab25.quizmaker.controller.teacher;
 
+import ir.maktab25.quizmaker.rest.QuestionResource;
 import ir.maktab25.quizmaker.rest.QuizResource;
+import ir.maktab25.quizmaker.service.dto.DescriptiveQuestionDTO;
+import ir.maktab25.quizmaker.service.dto.MultipleChoiceQuestionDTO;
 import ir.maktab25.quizmaker.service.dto.QuizDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,13 +19,29 @@ public class TeacherQuestionController {
     @Autowired
     QuizResource quizResource;
 
+    @Autowired
+    QuestionResource questionResource;
+
     @GetMapping("/{quizId}")
     public String showQuestions(@PathVariable Long quizId, Model model) {
         bindDataForTeacherQuestions(quizId, model);
         return "teacherQuestions";
     }
 
-    private void bindDataForTeacherQuestions(Long quizId, Model model){
+    @GetMapping("/addQuestionPage/{quizId}")
+    public String addQuestionPage(@PathVariable Long quizId, Model model) {
+        bindDataForAddQuestionPage(quizId, model);
+        return "addQuestion";
+    }
+
+    private void bindDataForAddQuestionPage(Long quizId, Model model) {
+        model.addAttribute("MultipleChoiceQuestion", new MultipleChoiceQuestionDTO());
+        model.addAttribute("DescriptiveQuestion", new DescriptiveQuestionDTO());
+        model.addAttribute("quizId", quizId);
+        model.addAttribute("questionBank", questionResource.findAllTeacherQuestions());
+    }
+
+    private void bindDataForTeacherQuestions(Long quizId, Model model) {
         QuizDTO quizDTO = quizResource.getById(quizId).getBody();
         model.addAttribute("questions", quizDTO.getQuestions());
         model.addAttribute("quizName", quizDTO.getTitle());
