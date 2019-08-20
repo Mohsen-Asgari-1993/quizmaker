724036c76ba080cf92d1ecb1b737d2952a22ec34
Mohsen <mohsen1993asgari@gmail.com>
2019-08-20 11:33:46 +0430

    Creates TeacherQuestionController bind teacherName and list of long to model

--- src/main/java/ir/maktab25/quizmaker/controller/teacher/TeacherQuestionController.java
+++ src/main/java/ir/maktab25/quizmaker/controller/teacher/TeacherQuestionController.java
@@ -1,7 +1,9 @@
 package ir.maktab25.quizmaker.controller.teacher;
 
+import ir.maktab25.quizmaker.base.util.CurrentUserDetail;
 import ir.maktab25.quizmaker.rest.QuestionResource;
 import ir.maktab25.quizmaker.rest.QuizResource;
+import ir.maktab25.quizmaker.rest.TeacherResource;
 import ir.maktab25.quizmaker.service.dto.DescriptiveQuestionDTO;
 import ir.maktab25.quizmaker.service.dto.MultipleChoiceQuestionDTO;
 import ir.maktab25.quizmaker.service.dto.QuizDTO;
@@ -10,7 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/teacher/question")
@@ -21,6 +27,9 @@ public class TeacherQuestionController {
 
     @Autowired
     QuestionResource questionResource;
+
+    @Autowired
+    TeacherResource teacherResource;
 
     @GetMapping("/{quizId}")
     public String showQuestions(@PathVariable Long quizId, Model model) {
@@ -34,11 +43,22 @@ public class TeacherQuestionController {
         return "addQuestion";
     }
 
+    @PostMapping("/addFromBank/{quizId}")
+    public String addFromBank(@PathVariable Long quizId, Model model, List<Long> longList) {
+
+
+        bindDataForTeacherQuestions(quizId, model);
+        return "teacherQuestions";
+    }
+
     private void bindDataForAddQuestionPage(Long quizId, Model model) {
         model.addAttribute("MultipleChoiceQuestion", new MultipleChoiceQuestionDTO());
         model.addAttribute("DescriptiveQuestion", new DescriptiveQuestionDTO());
         model.addAttribute("quizId", quizId);
-        model.addAttribute("questionBank", questionResource.findAllTeacherQuestions());
+        model.addAttribute("questionBank", questionResource.findAllTeacherQuestions().getBody());
+        model.addAttribute("idList", new ArrayList<Long>());
+        model.addAttribute("name", teacherResource.findAllByUsername(CurrentUserDetail.getCurrentUsername()).getBody().getLastName());
+
     }
 
     private void bindDataForTeacherQuestions(Long quizId, Model model) {
@@ -46,5 +66,7 @@ public class TeacherQuestionController {
         model.addAttribute("questions", quizDTO.getQuestions());
         model.addAttribute("quizName", quizDTO.getTitle());
         model.addAttribute("quizId", quizId);
+        model.addAttribute("name", teacherResource.findAllByUsername(CurrentUserDetail.getCurrentUsername()).getBody().getLastName());
+
     }
 }
