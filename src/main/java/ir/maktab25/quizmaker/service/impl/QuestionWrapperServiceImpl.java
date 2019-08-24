dca695b3efa43eb11d5c06c59689493ffdd6a220
Mohsen <mohsen1993asgari@gmail.com>
2019-08-24 12:44:30 +0430

    updates QuestionWrapperServiceImpl override save method

--- src/main/java/ir/maktab25/quizmaker/service/impl/QuestionWrapperServiceImpl.java
+++ src/main/java/ir/maktab25/quizmaker/service/impl/QuestionWrapperServiceImpl.java
@@ -3,7 +3,9 @@ package ir.maktab25.quizmaker.service.impl;
 import ir.maktab25.quizmaker.base.service.impl.BaseServiceImpl;
 import ir.maktab25.quizmaker.domain.QuestionWrapper;
 import ir.maktab25.quizmaker.repository.QuestionWrapperRepository;
+import ir.maktab25.quizmaker.service.QuestionService;
 import ir.maktab25.quizmaker.service.QuestionWrapperService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +14,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class QuestionWrapperServiceImpl extends BaseServiceImpl<QuestionWrapper, Long, QuestionWrapperRepository>
         implements QuestionWrapperService {
 
+    @Autowired
+    QuestionService questionService;
+
     public QuestionWrapperServiceImpl(QuestionWrapperRepository baseRepository) {
         super(baseRepository);
+    }
+
+    @Override
+    public QuestionWrapper save(QuestionWrapper t) {
+        questionService.save(t.getQuestion());
+        return super.save(t);
     }
 }
