ec4e5974688fed45e66035da38c39fe30f79b3a4
Mohsen <mohsen1993asgari@gmail.com>
2019-08-24 10:47:48 +0430

    Creates QuestionWrapperService

---
+++ src/main/java/ir/maktab25/quizmaker/service/QuestionWrapperService.java
@@ -0,0 +1,7 @@
+package ir.maktab25.quizmaker.service;
+
+import ir.maktab25.quizmaker.base.service.BaseService;
+import ir.maktab25.quizmaker.domain.QuestionWrapper;
+
+public interface QuestionWrapperService extends BaseService<QuestionWrapper, Long> {
+}
